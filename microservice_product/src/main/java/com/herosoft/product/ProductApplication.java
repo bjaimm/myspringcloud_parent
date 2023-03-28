b93395b7d0e1371ec950bd517e5f3a1cbe6a21a6
HW <bjaimm@hotmail.com>
2023-03-28 09:32:43 +0800

    update

--- microservice_product/src/main/java/com/herosoft/product/ProductApplication.java
+++ microservice_product/src/main/java/com/herosoft/product/ProductApplication.java
@@ -12,5 +12,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class,args);
+
     }
 }
--- microservice_product/src/main/java/com/herosoft/product/controller/ProductController.java
+++ microservice_product/src/main/java/com/herosoft/product/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.herosoft.product.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.herosoft.commons.dto.OrderDetailDto;
 import com.herosoft.commons.dto.ProductDto;
+import com.herosoft.commons.results.Result;
 import com.herosoft.product.po.ProductPo;
 import com.herosoft.product.service.impl.ProductServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -15,6 +19,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/products")
+@Slf4j
 public class ProductController {
     @Autowired
     private ProductServiceImpl productService;
@@ -44,4 +49,23 @@ public class ProductController {
         productService.save(product);
         return "添加成功";
     }
+
+    @RequestMapping(method = RequestMethod.PUT,value="/deduceStock",produces = "application/json")
+    @Transactional
+    public String deduceStock(@RequestBody List<OrderDetailDto> orderDetailDto){
+        log.info("收到还原库存请求。。。");
+        List<ProductPo> productPoList = orderDetailDto.stream()
+                .map(orderDetail -> {
+                    ProductPo productPo = productService.getById(orderDetail.getProductId());
+
+                    productPo.setProductAmount(productPo.getProductAmount() + orderDetail.getProductQty());
+
+                    return productPo;
+                })
+                .collect(Collectors.toList());
+
+        productService.updateBatchById(productPoList);
+
+        return "库存回滚成功";
+    };
 }
