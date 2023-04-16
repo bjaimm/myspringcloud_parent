6c9468eb2729470968ae1f6a7821e7a243a33ac0
HW <bjaimm@hotmail.com>
2023-04-16 18:40:05 +0800

    更新microsevice_product

---
+++ microservice_commons/src/main/java/com/herosoft/commons/dto/OrderDetailDto.java
@@ -0,0 +1,15 @@
+package com.herosoft.commons.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDetailDto {
+
+    private int orderDetailId;
+    private int productId;
+    private int productQty;
+}
